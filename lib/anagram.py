4752d05598140573a2ec00f3bea91fc68b8d738e
Hannah Bradley <hannahthesailor@yahoo.com>
2024-03-18 14:32:38 -0400

    python-p3-anagram-detector-lab.md

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        matches = []
+
+        for anagram in words:
+            if sorted(self.word) == sorted(anagram) and self.word != anagram:
+                matches.append(anagram)
+
+        return matches
